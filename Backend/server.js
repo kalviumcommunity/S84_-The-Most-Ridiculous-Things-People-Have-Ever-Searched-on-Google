2aa03e3b02eb4bee9d834215637a977ddd26be05
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-02-21 11:04:28 +0530

    Connected MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const connectToDb = require("./db");
 const app = express();
+require('dotenv').config();
 
-const PORT = 8080;
+app.use(express.json());
+
+const Port = process.env.PORT || 6969;
+const db_url = process.env.DB_URL 
 
 app.get("/", (req, res) => {
-    res.send("My name is Mayank Sharma. I am from squad-84")
+    res.send("<h2> My name is Mayank Sharma. I am from squad-84 </h2>")
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.get('/ping', (req, res) => {
+    res.send("<h1> I am currently inside ping destination! </h1>")
+})
+
+app.listen(Port, async() => {
+    try{
+        await connectToDb(db_url);
+        console.log(`Server is running on port http://localhost:${Port}`);
+        console.log(`Successfull connected to database with url as ${db_url}`)
+    }
+    catch(err){
+        console.log(err)
+    }
 })
