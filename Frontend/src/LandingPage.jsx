ceaf95185589f48a6c35c2648c3eab04f4638469
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-03-06 10:35:43 +0530

    Landing Page

---
+++ Frontend/src/LandingPage.jsx
@@ -0,0 +1,13 @@
+
+
+function LandingPage() {
+    return (
+        <>
+            <h1> Welcome to the most ridiculous things people have ever searched on Google! </h1>
+            <h2> Here you will find the most ridiculous things people have ever searched on Google. </h2>
+            <h3> Enjoy! </h3>
+        </>
+    );
+}
+
+export default LandingPage;
